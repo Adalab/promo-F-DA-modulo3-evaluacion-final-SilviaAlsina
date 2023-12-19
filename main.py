1f700b70fc2e258f253b9ad53b86472b38d4b795
salsi95 <silvials1995@gmail.com>
2023-12-20 00:44:09 +0100

    Examen terminado

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from src import soporte as sp
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
-from scipy.stats import ttest_ind, norm, chi2_contingency, f_oneway
 
 #%%
 '''
@@ -18,7 +16,7 @@ from scipy.stats import ttest_ind, norm, chi2_contingency, f_oneway
           valores nulos y estadísticas básicas de las columnas 
           involucradas.
         - Une los dos conjuntos de datos de la forma más 
-        eficiente.
+          eficiente.
     2. Limpieza de Datos:
         - Elimina o trata los valores nulos, si los hay, en 
           las columnas clave para asegurar que los datos estén
@@ -32,43 +30,56 @@ from scipy.stats import ttest_ind, norm, chi2_contingency, f_oneway
           análisis estadístico.
 '''
 #%%
+
 '''Primero llamo a la función del soporte para abrir los 
 archivos'''
 vuelos=sp.abrir_archivo('Customer Flight Activity.csv')
 clientes=sp.abrir_archivo('Customer Loyalty History.csv')
+
 #Ahora compruebo si hay filas duplicadas en ambos archivos
 print(f'clientes tiene los siguientes duplicados: {clientes.duplicated().sum()}')
 print(f'vuelos tiene los siguientes duplicados: {vuelos.duplicated().sum()}')
 
-'''Como podemos ver en la bbdd de activity sí que hay filas 
+'''Como podemos ver en la bbdd de vuelos sí que hay filas 
 duplicadas. Procedo a eliminarlas.'''
-#%%
 vuelos.drop_duplicates(inplace=True)
+
+#%%
+
 #A continuación, compruebo si los archivos tienen nulos
 print(f'VUELOS\n{vuelos.isnull().sum()}')
 '''Como podemos observar, en este primer archivo no 
 encontramos ningún nulo.'''
+
 print('-----------')
 print(f'CLIENTES\n{clientes.isnull().sum()}')
 '''En este segundo archivo sí que hay algún nulo. Vamos a 
 comprobar si es un porcentaje elevado o no.'''
+
 print(f'CLIENTES\n{clientes.isnull().sum()/clientes.shape[0]*100}')
 '''El porcentaje de nulos en estas columnas es bastante alto. 
    Por lo tanto, todas son firmes candidatas a ser eliminadas.'''
+
 #%%
+
 '''Ahora vamos a hacer un estudio general del tipo de datos
  de cada archivo'''
 print(vuelos.info())
-'''En el caso de activity vemos como la mayoría de los 
+'''En el caso de vuelos vemos como la mayoría de los 
 datos son int y hay un float. No hay nada raro aquí'''
+
 print(clientes.info())
-'''En el caso de history podemos ver como la columna postal 
+'''En el caso de clientes podemos ver como la columna postal 
 code es un object. Veamos si tiene sentido.'''
+
 print(clientes['Postal Code'].unique())
 '''Como podemos observar, los valores tienen letras y números,
  por lo que el tipo de dato es correcto'''
+
 # %%
-#Ahora vamos a comprobar cómo son los valores de las columnas del archivo de activity
+
+'''Ahora vamos a comprobar cómo son los valores de las columnas del 
+archivo de vuelos'''
 sp.contar_valores(vuelos,vuelos.columns.tolist())
 '''Loyalty number: Hay un valor predominante 678205.
    Year: Sólo tenemos 2017 y 2018
@@ -81,7 +92,7 @@ sp.contar_valores(vuelos,vuelos.columns.tolist())
    Points Redeemed: 0 es el valor predominante
    Dollar Cost Points Redeemed: 0 es el valor predominante'''
 #%%
-#Haremos el mismo proceso para history
+#Haremos el mismo proceso para clientes
 sp.contar_valores(clientes,clientes.columns.tolist())
 '''Loyalty number: Todos los valores son únicos
    Country: Sólo tenemos Canada
@@ -98,41 +109,55 @@ sp.contar_valores(clientes,clientes.columns.tolist())
    Enrollment Month:Predomina 5, pero por poco
    Cancellation Year: Predomina 2018, pero poco
    Cancellation Month: predominan 12,11 y 8'''
-'''Por lo que podemos observar, el archivo history contiene 
+
+'''Por lo que podemos observar, el archivo clientes contiene 
 los datos de Viajeros de una compañía de viajes canadiense, 
-mientras que el archivo activity muestra los viajes que ha 
+mientras que el archivo vuelos muestra los viajes que ha 
 tenido cada persona con dicha compañia. (Por eso tiene valores
  duplicados en Loyalty number)'''
+
 # %%
+
 '''Ahora que ya conozco la estructura de los datos, voy a 
 proceder a la unión de las tablas. Esta unión la voy a hacer 
 por la columna 'Loyalty Number', ya que es el id de cada 
 cliente. Voy a realizar un merge tipo inner ya que me 
 interesan los viajeros que tengamos en ambas bases de datos'''
 datos=vuelos.merge(clientes, how='inner', on='Loyalty Number')
+
 #%%
+
 '''Ahora que ya tengo un df con todos los datos voy a 
 estandarizar los nombres de las columnas y los datos.'''
 sp.nombres_col(datos)
 sp.datos_minus(datos,datos.columns.to_list())
+
 #%%
-'''A continuación procedo a gestionar los nulos qu nos hemos 
-traído desde la bbdd de history
+
+'''A continuación procedo a gestionar los nulos que nos hemos 
+traído desde la bbdd de clientes
 Nuestras columnas con alto porcentaje de nulos eran
  Cancellation Year y Cancelation Month, esto puede ser debido 
  a que no han cancelado la fidelidad con la empresa, por lo 
  que voy a modificar esos nulos'''
 datos['cancellation_year'].fillna('uncancelled',inplace=True)
 datos['cancellation_month'].fillna('uncancelled',inplace=True)
+
 # %%
+
 #Voy a eliminar los valores negativos de la columna salary
 datos['salary'] = datos['salary'].apply(lambda x: np.nan if x < 0 else x)
 
 # %%
+
 '''AHora voy a hacer un boxplot para ver si tiene sentido 
 cambiar los nulos por la media o la mediana.'''
-sns.boxplot(x = 'salary', data = datos)
+sns.boxplot(x = 'salary', data = datos, palette='cool_r')
+plt.xlabel('Salario')
+plt.title('Boxplot del Salario', fontsize = 15)
+
 #%%
+
 '''Como podemos observar en el boxplot, tiene muchísimos 
 outliers, por lo tanto, lo más óptimo será sustituir los 
 nulos por 'unknown'''
@@ -144,12 +169,14 @@ print(datos.isnull().sum())
 
 
 # %%
+
 '''· Fase 2: Visualización
 Usando las herramientas de visualización que has aprendido 
 durante este módulo, contesta a las siguientes preguntas 
 usando la mejor gráfica que consideres:'''
 
 # %%
+
 '''1. ¿Cómo se distribuye la cantidad de vuelos reservados 
 por mes durante el año?'''
 sns.lineplot(x='month',y='flights_booked', data=datos,palette='cool_r')
@@ -162,7 +189,9 @@ del mes de marzo. Teniendo en cuenta que todos los clientes son
  interrupción de las clases llamada 'Mid Winter Break'. Luego 
  el siguiente pico se encuentra en los meses de verano y el 
  último correspondería a Navidad.'''
+
 # %%
+
 '''2. ¿Existe una relación entre la distancia de los vuelos y 
 los puntos acumulados por los clientes?'''
 sns.scatterplot(x='distance',y='points_accumulated',data=datos, marker = ".",s=100, palette='cool_r')
@@ -171,7 +200,9 @@ plt.ylabel('puntos acumulados')
 plt.title('relación puntos/distancia', fontsize = 15)
 '''Vemos 4 líneas diferentes. Puede ser debido al tipo de 
 tarjeta de fidelidad'''
+
 #%%
+
 sns.scatterplot(x='distance',y='points_accumulated', hue='loyalty_card',data=datos, marker = ".",s=100, palette='cool_r')
 plt.xlabel('distancia')
 plt.ylabel('puntos acumulados')
@@ -179,7 +210,9 @@ plt.title('relación puntos/distancia', fontsize = 15)
 #plt.legend({'Aurora':'aurora','Nova':'nova','Star':'star'})
 '''Aquí podemos ver como efectivamente depende del tipo de 
 tarjeta, pero también de otros factores. Probemos con el año'''
+
 #%%
+
 fig, axes = plt.subplots(nrows = 1, ncols = 2, figsize = (20, 5))
 sp.visualizacion('distance','points_accumulated','loyalty_card',datos[datos['year']==2017],axes[0])
 axes[0].set_xlabel('distancia')
@@ -192,7 +225,9 @@ axes[1].set_title('relación puntos/distancia en 2018', fontsize = 15)
 '''Parece que en todo 2017 siguió una única tendencia lineal, pero 
 en 2018 no fue así. Probemos ahora a separar por meses la gráfica 
 de 2018'''
+
 #%%
+
 dos_18=datos[datos['year']==2018]
 fig, axes = plt.subplots(nrows = 6, ncols = 2, figsize = (20, 50))
 sp.visualizacion('distance','points_accumulated','loyalty_card',dos_18[dos_18['month']==1],axes[0,0])
@@ -246,7 +281,9 @@ axes[5,1].set_title('relación puntos/distancia en diciembre del 2018', fontsize
 '''Como podemos ver, para algunos meses la tendencia lineal es 
 independiente del tipo de tarjeta, pero para otros sí que es 
 dependiente'''
+
 #%%
+
 '''Para todos los casos vistos vamos a comprobar la linealidad'''
 dos_17=datos[datos['year']==2017]
 print('Para el año 2017')
@@ -262,7 +299,9 @@ for i in range (1,13):
         for unique in loyalty:
             print(f'Para el año 2018, mes {i} y tarjeta tipo {unique}:')
             sp.identificar_linealidad(mes[mes['loyalty_card']==unique],'distance','points_accumulated')
+
 # %%
+            
 '''3. ¿Cuál es la distribución de los clientes por provincia o 
 estado?'''
 plt.hist(x = 'Province', data =clientes, color = "turquoise", edgecolor = "white")
@@ -272,12 +311,13 @@ plt.ylabel('Clientes')
 plt.title('distribución clientes/provincia', fontsize = 15)
 '''Como podemos observar, la mayoría de los clientes son de las 
 provincias de Ontario, British Columbia y Quebec'''
+
 #%%
+
 '''4. ¿Cómo se compara el salario promedio entre los diferentes
  niveles educativos de los clientes?'''
 '''Voy a crear un nuevo df donde descartemos las filas con 
 salarios desconocidos'''
-#%%
 clientes['Salary'].fillna('unknown',inplace=True)
 salario=clientes[clientes['Salary']!='unknown']
 salario=salario.groupby('Education')['Salary'].mean().reset_index()
@@ -288,21 +328,41 @@ plt.xlabel('Educación')
 plt.ylabel('Salario')
 plt.title('Comparación salario/educación', fontsize = 15)
 
+'''Como podemos observar, a medida que aumenta el nivel educativo
+aumenta igualmente el salario medio. Además, el grupo de college
+ha desaparecido. Eso puede ser porque probablemente sean clientes
+que aún están en edad escolar y no tienen trabajo.'''
+
 # %%
+
 '''5. ¿Cuál es la proporción de clientes con diferentes tipos de 
 tarjetas de fidelidad?'''
 sns.countplot(x = 'Loyalty Card', data = clientes, palette='cool_r')
 plt.xlabel('Tarjeta Fidelidad')
 plt.ylabel('Clientes')
 plt.title('Proporción Tarjeta/Clientes', fontsize = 15)
+'''Hay una mayor cantidad de Star, aeguido de Nova y por último
+Aurora'''
+
 # %%
+
 '''6. ¿Cómo se distribuyen los clientes según su estado civil y 
 género?'''
 sns.countplot(x = 'Marital Status', data = clientes,hue='Gender',palette='cool_r')
 plt.xlabel('Estado Civil')
 plt.ylabel('Clientes')
 plt.title('Proporción Clientes/Estado Civil', fontsize = 15)
+'''Hay una mayor cantidad de clientes casados, seguidos de solteros
+y por último, divorciados. En el caso de solteros y divorciados 
+hay una muy pequeña diferencia entre hombres y mujeres'''
+
+
+
+
+
+
 # %%
+
 '''· Fase 3: Evaluación de Diferencias en Reservas de Vuelos por 
 Nivel Educativo.
 Objetivo del Ejercicio: Utilizando un conjunto de datos que hemos 
@@ -321,24 +381,58 @@ clientes. Para ello, los pasos que deberas seguir son:
         - Realiza una prueba de A/B testing para determinar si 
         existe una diferencia significativa en el número de vuelos 
         reservados entre los diferentes niveles educativos.'''
-# %%
-datos['group']=datos['education'].apply(sp.categorizar_grupos)
-# %%
-vuelos_por_persona=datos.groupby('loyalty_number')['flights_booked'].sum().reset_index()
-grupo_por_persona=datos[['loyalty_number','group']].drop_duplicates()
-# %%
-vuelos_por_persona=vuelos_por_persona.merge(grupo_por_persona, how='inner', on='loyalty_number')
-# %%
-sp.normalidad(vuelos_por_persona,'flights_booked')
 
 # %%
-sp.homogeneidad(vuelos_por_persona,'group','flights_booked')
+
+#Primero creo una columna con los grupos
+datos['group']=datos['education'].apply(sp.categorizar_grupos)
+'''Ahora creo un df con el número total de vuelos por persona y el grupo
+al que pertenecen cada  uno'''
+
+vuelos_por_persona=datos.groupby('loyalty_number')['flights_booked'].sum().reset_index()
+grupo_por_persona=datos[['loyalty_number','group']].drop_duplicates()
+#Ahora uno las tablas para crear una sola
+vuelos_por_persona=vuelos_por_persona.merge(grupo_por_persona, how='inner', on='loyalty_number')
+
+# %%
+
+'''Con .describe podemos ver los principales estadísticos de la 
+columna flights_booked'''
+vuelos_por_persona['flights_booked'].describe().T
+
 #%%
-vuelos_por_persona.groupby('group')['loyalty_number'].count().reset_index()
+
+'''Ahora voy a comenzar el A/B testing empezando por la definición 
+de las hipótesis.
+· Hipotesis nula[HO]-->No hay cambios significativos entre los dos 
+grupos
+· Hipotesis alternativa [H1]-->Hay cambios significativos entre 
+los dos grupos.
+Compruebo si sigue una distribución normal. Uso el test de 
+kolmogorov, ya que la muestra era de un tamaño mayor de 50'''
+sp.normalidad(vuelos_por_persona,'flights_booked')
+sns.histplot(x = 'flights_booked', hue='group',data = vuelos_por_persona,palette='cool_r')
+plt.xlabel('Vuelos Reservados')
+plt.ylabel('Clientes')
+plt.title('Proporción Clientes/Vuelos Reservados', fontsize = 15)
+
 # %%
+
+'''Aunque se ha comprobado que NO siguen una distribución normal 
+voy a comprobar la homogeneidad de varianzas utilizando el test de 
+Levenne, ya que el de Bartet es más sensible a la falta de 
+normalidad '''
+sp.homogeneidad(vuelos_por_persona,'group','flights_booked')
+
+#%%
+
+'''Ahora deberíamos elegir el test estadístico idóneo para el A/B
+testing. En este caso, al no tener una distribución normal haré un
+test de Mann-Whitney'''
 sp.test_man_whitney(vuelos_por_persona,'flights_booked','grupo A','grupo B','group')
-# %%
-sns.histplot(x = 'flights_booked', hue='group',data = vuelos_por_persona)
-# %%
-sp.ab_testing_2(vuelos_por_persona,'group','flights_booked',)
-# %%
+'''Co el resultado del análisis, al no haber diferencias 
+significativas en las medianas de ambos grupos podemos dar
+por válida la hipótesis nula, es decir, que no hay una relación 
+aparente entre el número de vuelos reservados y el nivel educativo
+de los clientes'''
+
--- src/soporte.py
+++ src/soporte.py
@@ -1,10 +1,8 @@
 #%%
 import pandas as pd
-import numpy as np
-from scipy.stats import kstest,norm,shapiro,levene
+from scipy.stats import kstest,levene
 import scipy.stats as st
 import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 #%%
 def abrir_archivo (nombre):
@@ -14,16 +12,11 @@ def contar_valores(df,columnas):
     for columna in columnas:
         print(df[columna].value_counts())
         print('----------')
-def eliminar_col (df,col):
-    return df.drop(col, axis=1,inplace=True)
-def eliminar_negativos (valor):
-    if valor<0:
-        return np.nan
-    else:
-        pass
+
 def nombres_col(df):
     nuevas_columnas = {columna: columna.lower().replace(' ','_') for columna in df.columns}
     return df.rename(columns=nuevas_columnas, inplace= True)
+
 def datos_minus(df,columnas):
     for col in columnas:
         try:
@@ -33,79 +26,21 @@ def datos_minus(df,columnas):
 
 def visualizacion (x,y,group,data,axes):
     sns.scatterplot(x=x,y=y, hue=group,data=data, marker = ".",s=100, palette='cool_r',ax=axes)
+
 def identificar_linealidad (df, col1,col2): 
     X = df[[col1]]
     y = df[col2]
     modelo_regresion = LinearRegression()
     modelo_regresion.fit(X, y)
     print(f"Puntaje R^2: {modelo_regresion.score(X, y):.4f}")
+
 def categorizar_grupos(educacion):
     if educacion == 'college' or educacion == 'high school or below':
         return "grupo B"
     else:
         return "grupo A"
     
-
-def ab_testing(df,columna1,columna2):
-
-    df_grupoA = df[df[columna1]== "grupo A"]
-    df_grupoB = df[df[columna1]== "grupo B"]
-    
-    df_2=df.groupby(columna1)[columna2].sum().reset_index()
-    # hacemos un análisis visual previo
-    sns.barplot(hue=columna1, y=columna2, data=df_2,  palette = "cool_r")       
-    # Calcular la media y la desviación estándar del grupo control
-    media_A_z = df_grupoA[columna2].mean()
-    std_A_z = df_grupoA[columna2].std()
-    print(media_A_z)
-    print(std_A_z)
-    # Calcular la media y la desviación estándar del grupo control
-    media_B_z = df_grupoB[columna2].mean()
-    std_B_z = df_grupoB[columna2].std()
-    print(media_B_z)
-    print(std_B_z)
-
-    # Calcular la cantidad de datos que tenemos en el grupo control y el test
-    n_A_z = len(df_grupoA)
-    print(n_A_z)
-    n_B_z = len(df_grupoB)
-    print(n_B_z)
-
-    # calcular el valor de la z
-    z_stat = (media_B_z - media_A_z) / np.sqrt((std_A_z**2 / n_A_z) + (std_B_z**2 / n_B_z))
-
-    # Calcular el valor p
-    p_value = 2 * (1 - norm.cdf(np.abs(z_stat)))
-
-    # Imprimir el resultado de la prueba
-    alpha = 0.05
-    if p_value < alpha:
-        print("Hay una diferencia significativa en el número de vuelos entre el grupo A y el grupo B.")
-        print("\n ---------- \n")
-        print("""
-            Los resultados sugieren que si que existe una diferencia entre el número de vuelos entre el grupo A y B,
-            por lo que tendremos quee optar por la nueva versión de la página web
-            """)
-    else:
-        print("No hay evidencia de una diferencia significativa en el número de vuelos entre el grupo A y el grupo B.")
-        print("\n ---------- \n")
-        print(""" 
-            Los resultados sugieren que no existe evidencia estadística para afirmar que las medias de las muestras son distintas,
-            por lo que la nueva campaña no esta ayudando a nuestro problema. 
-            """)
-        
 def normalidad(dataframe, columna):
-    """
-    Evalúa la normalidad de una columna de datos de un DataFrame utilizando la prueba de Shapiro-Wilk.
-
-    Parámetros:
-        dataframe (DataFrame): El DataFrame que contiene los datos.
-        columna (str): El nombre de la columna en el DataFrame que se va a evaluar para la normalidad.
-
-    Returns:
-        None: Imprime un mensaje indicando si los datos siguen o no una distribución normal.
-    """
-
     statistic, p_value = st.kstest(dataframe[columna],'norm')
     if p_value > 0.05:
         print(f"Para la columna {columna} los datos siguen una distribución normal.")
@@ -113,22 +48,6 @@ def normalidad(dataframe, columna):
         print(f"Para la columna {columna} los datos no siguen una distribución normal.")
 
 def homogeneidad (dataframe, columna, columna_metrica):
-    
-    """
-    Evalúa la homogeneidad de las varianzas entre grupos para una métrica específica en un DataFrame dado.
-
-    Parámetros:
-    - dataframe (DataFrame): El DataFrame que contiene los datos.
-    - columna (str): El nombre de la columna que se utilizará para dividir los datos en grupos.
-    - columna_metrica (str): El nombre de la columna que se utilizará para evaluar la homogeneidad de las varianzas.
-
-    Returns:
-    No devuelve nada directamente, pero imprime en la consola si las varianzas son homogéneas o no entre los grupos.
-    Se utiliza la prueba de Levene para evaluar la homogeneidad de las varianzas. Si el valor p resultante es mayor que 0.05,
-    se concluye que las varianzas son homogéneas; de lo contrario, se concluye que las varianzas no son homogéneas.
-    """
-    
-    # lo primero que tenemos que hacer es crear tantos conjuntos de datos para cada una de las categorías que tenemos, Control Campaign y Test Campaign
     valores_evaluar = []
     
     for valor in dataframe[columna].unique():
@@ -142,31 +61,12 @@ def homogeneidad (dataframe, columna, columna_metrica):
 
 
 def test_man_whitney(dataframe,metrica, grupo_A, grupo_B, columna_grupos):
-
-    """
-    Realiza la prueba de Mann-Whitney U para comparar las medianas de las métricas entre dos grupos en un DataFrame dado.
-
-    Parámetros:
-    - dataframe (DataFrame): El DataFrame que contiene los datos.
-    - columnas_metricas (list): Una lista de nombres de columnas que representan las métricas a comparar entre los grupos.
-    - grupo_control (str): El nombre del grupo de control en la columna especificada por columna_grupos.
-    - grupo_test (str): El nombre del grupo de test en la columna especificada por columna_grupos.
-    - columna_grupos (str): El nombre de la columna que contiene la información de los grupos. Por defecto, "campaign_name".
-
-    Returns 
-    No devuelve nada directamente, pero imprime en la consola si las medianas son diferentes o iguales para cada métrica.
-    Se utiliza la prueba de Mann-Whitney U para evaluar si hay diferencias significativas entre los grupos.
-    """
-    # filtramos el DataFrame para quedarnos solo con los datos de grupoA
     grupoA = dataframe[dataframe[columna_grupos] == grupo_A]
-    
-    # filtramos el DataFrame para quedarnos solo con los datos de control
     grupoB = dataframe[dataframe[columna_grupos] == grupo_B]
     
     metrica_grupoA = grupoA[metrica]
     metrica_grupoB = grupoB[metrica]
-    # iteramos por las columnas de las metricas para ver si para cada una de ellas hay diferencias entre los grupos
-            # aplicamos el estadístico
+    
     u_statistic, p_value = st.mannwhitneyu(metrica_grupoA, metrica_grupoB)
     
     if p_value < 0.05:
